43359f18b52b50afe16443fc310f68ffd5e3d227
Rafa-77 <alex_brandi77@hotmail.com>
2024-07-16 23:18:50 -0600

    new file:   _MyWebSites/Equisoft01/01/index.html 	new file:   _MyWebSites/Equisoft01/01/scripts.js 	new file:   _MyWebSites/Equisoft01/01/styles.css 	new file:   _MyWebSites/Equisoft01/02/index.html 	new file:   _MyWebSites/Equisoft01/02/scripts.js 	new file:   _MyWebSites/Equisoft01/02/styles.css

---
+++ _MyWebSites/Equisoft01/01/scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Add any JavaScript you need here
+    console.log("JavaScript is loaded and working!");
+
+    // Optional: If you want to add some interactive features or animations
+});
