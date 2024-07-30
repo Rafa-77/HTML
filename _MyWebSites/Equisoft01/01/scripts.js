7cb12f7f5e80620018a2b8b5fcf78845a611c508
Rafa-77 <alex_brandi77@hotmail.com>
2024-07-30 00:02:39 -0600

    new file:   _MyWebSites/Equisoft01/01/Images/Hero.jpg 	new file:   _MyWebSites/Equisoft01/01/Images/Logo.jpg 	modified:   _MyWebSites/Equisoft01/01/index.html 	modified:   _MyWebSites/Equisoft01/01/scripts.js 	modified:   _MyWebSites/Equisoft01/01/styles.css 	new file:   _MyWebSites/Equisoft01/02/Images/Hero.jpg 	new file:   _MyWebSites/Equisoft01/02/Images/Logo.jpg 	modified:   _MyWebSites/Equisoft01/02/index.html 	modified:   _MyWebSites/Equisoft01/02/styles.css

--- _MyWebSites/Equisoft01/01/scripts.js
+++ _MyWebSites/Equisoft01/01/scripts.js
@@ -1,6 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Add any JavaScript you need here
     console.log("JavaScript is loaded and working!");
-
-    // Optional: If you want to add some interactive features or animations
 });
